1d0d713f85957feebf8a641e509cc8512e8311c6
rahulnvaidya <rahulnvaidya@001b6fc9-6b4e-0410-ae3c-1dc1a7f4b225>
2008-05-30 05:32:29 +0000

    Initial commit.

---
+++ trunk/NEATRacing/RacingGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Racing Game")]
+[assembly: AssemblyProduct("Racing Game")]
+[assembly: AssemblyDescription("SOLUTION Racing Game is a 3D auto racing game. Race around the track and try to beat the ghost car to achieve the best time.")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// 0.7: earlier versions
+// 0.8: Alpha
+// 0.9: Beta
+// 1.0: Final
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: Guid("b7088bd4-9ca1-431f-9bab-269e3e1167b6")]
